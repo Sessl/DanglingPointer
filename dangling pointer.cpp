f1a34bda798903ed287869d9ccecaaf572110689
Suchitra Edussuriya <sse_p@hotmail.com>
2013-08-04 23:24:13 -0700

    Dangling Pointer Example in C++

--- dangling pointer.cpp
+++ dangling pointer.cpp
@@ -10,13 +10,13 @@ int main()
 	char myLine[100];
 
 	p = new (nothrow) int;   //allocate new block of memory that is of type integer and assign address to pointer
-   	*p = 10;                 //initialise the variable p points to with the value 10
+   	*p = 10;                 //initialise the variable p points to, with the value 10
 	cout << p << endl;
 	cout << *p << endl;
 	cin.getline(myLine,100);
 	delete p;               //free the block of memory p points to. 
     cout << p << endl;      // p stills holds the address of the block of memory it was previously pointing to
-	cout << *p << endl;     // p now points to a value to a undefined value. Thus p is "dangling pointer"
+	cout << *p << endl;     // p now points to an undefined value. Thus p is a "dangling pointer"
 	cin.getline(myLine,100);
    
 	return 0;
